f89baafc0dc9ad617041349439b6e1350e11069e
Juan Ramon <jlastd00@estudiantes.unileon.es>
2023-02-03 23:04:40 +0100

    fix connection db.

--- server/utils/database.js
+++ server/utils/database.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
+import { print, printError } from "./functions.js";
 import { DB_URI } from "./properties.js";
 
-mongoose.set('strictQuery', false);
+mongoose.set('strictQuery', false); 
 
 mongoose.connect(DB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-})
-    .then(db => console.log('Connected to database:', db.connection.name))
-    .catch(err => console.error(err));
+})  
+    .then(db => print(`Connected to database: ${db.connection.name}`))
+    .catch(err => printError(err));
+
