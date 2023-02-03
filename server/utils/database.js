5bb0851212921d52eadce1b2147a10322b54d9bf
Juan Ramon <jlastd00@estudiantes.unileon.es>
2023-02-03 22:38:39 +0100

    Fix connection mongodb corregido.

--- server/utils/database.js
+++ server/utils/database.js
@@ -3,6 +3,9 @@ import { DB_URI } from "./properties.js";
 
 mongoose.set('strictQuery', false);
 
-mongoose.connect(DB_URI, {})
+mongoose.connect(DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
     .then(db => console.log('Connected to database:', db.connection.name))
     .catch(err => console.error(err));
